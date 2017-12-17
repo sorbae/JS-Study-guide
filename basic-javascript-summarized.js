b52dc69da507b6055ed24ab46b3baa0b9d980656
Phoebe Mei <meipbe@gmail.com>
2017-12-16 21:00:06 -0800

    Condensed JavasScript Functions

---
+++ basic-javascript-summarized.js
@@ -0,0 +1,336 @@
+
+function.prototype.call(thisArg, arg1, arg2, ...)
+Array.prototype.filter.call([1, 2, 3], function() { })
+// multiple independent arguments
+
+function.prototype.apply(thisArg, [argsArray])
+Array.prototype.filter.apply([1, 2, 3], [function() { }])
+// an array of arguments
+
+Array.prototype.function()
+
+Object.getOwnPropertyNames(obj)
+Object.keys(obj)
+//returns array of object keys
+
+Object.values(obj)
+//returns object values
+
+for (let i in obj)
+//loop objs
+
+Object.entries(obj)
+// returns array of keys and values
+// [ ['0', 'a'], ['1', 'b'], ['2', 'c'] ]
+
+array.entries()
+// returns array of key/value pairs for each index in the array
+// [[0, 'a'], [1, 'b'], [2, 'c']]
+
+Object.assign(target, sources)
+// merges objects to target; if keys are shared, value will be replaced with last var's key value
+// target can equal empty obj {}
+// let o1 = { a: 1, b: 1, c: 1 };
+// let o2 = { b: 2, c: 2 };
+// let o3 = {c: 3};
+// var obj = Object.assign(o1, o2, o3) //{a: 1, b: 2, c: 3};
+// console.log(o1) //{a: 1, b: 2, c: 3};
+
+obj.hasOwnProperty(key)
+// returns a boolean indicating whether or not the object has the specified property as own property.
+
+delete obj[key]
+// deletes obj property
+
+/******************************/
+
+Array.isArray(array)
+// returns boolean
+
+obj instanceof constructor
+// ex: obj[key] instanceof Array
+// The instanceof operator tests presence of constructor.prototype in object's prototype chain.
+
+for (let i of array)
+// loop over arrays & strings
+
+num.toString().split('')
+// Split numbers into separate digits with indices
+
+let arrOfWords = str.split(' ');
+const setOfUniqueWords = new Set(arrOfWords);
+// set requires an ARRAY to be created
+// creates an OBJECT
+
+str1.concat(str2, str3)
+array1.concat(array2, array3)
+// does not modify the original arrays/strings
+
+array.copyWtihin(target, start, end)
+// target refers to the 0 based index at which to copy the sequence
+// start & end refers what is to be copied
+// negative numbers will be counted from the end
+
+str.repeat(num)
+// no spaces included
+
+array.unshift(element)
+// ADDS element to FRONT of array
+
+array.shift()
+// REMOVES element from FRONT of array
+// returns the element removed
+
+array.push(element)
+// ADDS element to BACK of array
+
+array.pop()
+// REMOVES element from BACK of array
+// returns the element removed
+
+array.slice(start, end)
+str.slice(start, end)
+
+slice(1)
+// removes the first index & returns the rest of the array/string
+// shift() does the same but only for ARRAYS
+
+slice(-1)
+// returns the last index of the array/string
+
+slice(0, -1)
+// returns string/array with the last index removed
+// pop() does the same but only for ARRAYS
+
+array.slice() //creates a copy of the array
+// creates a new array & string, not including the last index
+
+array.splice(start, deleteCount, insertRandomShitHere)
+
+array.includes(element)
+string.includes('hello')
+// returns booleans
+
+array.find(predicateFun)
+// returns value of FIRST element in array that passes the predicate function
+// otherwise returns undefined
+
+array.findIndex(predicateFun)
+// returns index of the FIRST value in array that passes predicate function
+// otherwise returns -1
+
+array.lastIndexOf(searchElement)
+// returns the last index at which a given element can be found in the array
+// starts search from the end of the array
+// otherwise returns -1
+
+array.fill(num, startIndex, endIndex)
+// returns array filled with num starting between startIndex & endIndex
+// startIndex defaults to zero if not set
+// endIndex defaults to array.length if not set
+
+Array.of(setOfElements) //Array.of(7) --> [7]
+// setOfElements cannot be a variable with the set of lements
+// the set of elements (string/numbers) must be inserted directly into the argument
+// can take in arrays, objects, and functions to produce an array of its arguments
+
+Array.from(arguments)
+Array.from(array)
+// creates a copy of the array
+Array.from(string) //creates an array out of each string character
+// same as str.split('')
+
+// Array.from() has an optional parameter mapFn: Array.from(obj, mapFn, thisArg)
+// has the same result as Array.from(obj).map(mapFn, thisArg) except it doesn't create an intermediate array
+
+/********************************/
+// STRING --> SPLIT --> CREATES ARRAY
+
+str.split()
+// returns entire str in array
+
+str.split('')
+// returns ['s','t','r']
+
+/*******************************/
+// ARRAY --> JOIN --> CREATES STRING
+
+array.join()
+array.toString()
+// 'Wind,Rain,Fire'
+// no spaces & joined by a comma
+
+array.join('')
+// 'WindRainFire'
+
+array.join(' ')
+// 'Wind Rain Fire'
+// concat does not create space
+
+/********************************/
+
+Math.pow(base, exponent)
+
+Math.sqrt(num)
+
+Math.cbrt(num)
+
+Math.abs(num)
+
+Math.floor(num)
+
+Math.ceil(num)
+
+Math.random()
+
+Math.round(num)
+// returns value of number rounded to nearest integer
+
+Math.fround(num)
+// returns nearest single precision float number
+
+Math.max(1, 2, 3)
+Math.min(1, 2, 3)
+// use apply for arrays --> Math.max.apply(Math, array)
+// returns NaN if at least one arg cannot be parsed into a number
+
+Number.isInteger(num)
+// return boolean (whole number)
+
+parseInt('string', 10)
+// string --> whole number
+Number('string')
+(+'stringNumber')
+// string --> number
+
+obj.toString(10)
+// number --> string
+// obj --> string
+// array --> string (separated by ,)
+
+
+num.toFixed(number)
+// The number of digits to appear after the decimal point (rounded)
+// returns a string
+
+/********************************/
+
+typeof string === 'string'
+typeof boolean === 'boolean'
+typeof number === 'number'
+typeof functionObj === 'function'
+typeof obj === 'obj'
+
+/****************REGULAR EXPRESSIONS****************/
+
+** NOTE: Use backslashes to grab literal characters **
+
+META-CHARACTERS:
+\d //any digit 0 - 9
+\w //word: capital A - Z, lowercase a - z, 0 - 9
+\W //anything that's not a word character (i.e. \w)
+.  //any character what so ever (NOTE: \. means literal dot)
+*  //wild-card quantifier: 0 or more
+.* //common way to match file names (i.e. rainbow.*)
+\s //any white space (i.e. space, tab)
+\S //anything that's not white space (i.e. \s)
+
+
+QUANTIFIERS:
+//meta-characters that modify the previous character in a regular expression & say how many of those things you want in a row
+*  //0 or more
++  //1 or more
+?  //0 or 1 EX: (searching for: colou?rs? --> color, colors, colour, colours)
+{min, max}
+{n} //(i.e. \w{5}\s --> words with 5 characters and a space)
+
+POSITION:
+//meta-characters that matches the position of a character in the string itself
+^  //beginning of a line (i.e. ^\w+ --> words at the beginning of a line with one or more characters)
+$  //end of a line (i.e. ^\w+$ --> searches for lines that only have one word)
+\b //word boundary (i.e. \b\w{4, 6}\b --> words with 4-6 characters)
+
+CHARACTER CLASSES:
+//stuff that appears in between [] are all literatals acting as an ALTERNATION function: having an OR-like operation
+[abc] --> matches a, b, or c
+[-.]  --> matches dash or dot (. is no longer a meta-character within [], but a dot)
+
+Special characters in character classes:
+-  // [a-z], if the dash is not the first character, it does NOT mean a literal character
+^  // [^0-5] if the carrot is the FIRST character within the bracket, it matches everything EXCEPT whatever is in the bracket
+
+[A-za-z]+ --> Any lowercase or uppercase character followed by 1 or more characters
+\b[A-z][a-z]*\b --> Matches words that have their first letter capitalized
+//EX: to match all three of the following phone numbers: 312-884-3234, (231)234-1259, 321.343.3234
+//You can use the following regEx: \(?\d{3}[-.)]\d{3}[-.]\d{4}
+
+(|) //plays similar role in alternation
+[\w.]+@\w+\.(net|com|edu) --> match emails
+
+
+/*****************SCOPES & CLOSURES***********************/
+Declarations (.i.e. variables and functions) are hoisted to the top due to the compilation step
+//function expressions return undefined if called before it is able to be executed
+// Ex:  x();
+// var x = function();
+
+Scopes are created by functions
+
+
+exampleObj = {
+  var a = 10;
+
+  function outer() {
+    var b = 20;
+
+    var inner = function() {
+      a++;
+      b++;
+      console.log(a);
+      console.lob(b);
+    };
+    return inner;
+   }
+}
+
+var innerFun = outer();
+
+innerFun(); //returns 11, 21;
+
+var innerFn2 = outer();
+
+innerFn2(); // returns 12, 21
+// var b remains the same because each time the function is called, a copy of
+// the function's variables is created at the time it is being called
+
+
+// This logs 10 copies of '10'
+// setTimeout is priting what its scope remembers:
+// At the end of the loop, the last value of i the scope remembers is 10
+// and therefore prints '10' 10 times.
+
+exampleObj2 = {
+  var 1;
+  var print = function() {
+    console.log(i);
+  };
+  for (i = 0; i < 10; i++) {
+    setTimeout(print, 1000);
+  }
+}
+
+// This logs 1, 2, 3... 10
+// A function is created to keep track of the value i so that
+// setTimeout can refer to it
+
+exampleObj2 = {
+  var 1;
+  for (i = 0; i <10; i++) {
+    (function() {
+      var currentValueOfI = i;
+      setTimeout(function() {
+        console.log(currentValueOfI);
+      }, 1000);
+    })();
+  }
+}
